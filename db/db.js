1956962edcc20a5a4f1aed95f15c3481d67f8840
silvabacc <47926269+silvabacc@users.noreply.github.com>
2022-12-06 12:16:16 +0000

    1 binaries uploaded

--- db/db.js
+++ db/db.js
@@ -4,15 +4,15 @@ import fs from 'fs';
 // setup a new database
 const sequelize = new Sequelize('database', '', '', {
   dialect: 'sqlite',
-  storage: '.data/database.sqlite',
+  storage: 'data/database.sqlite',
   logging: false,
 });
 
 const initSql = fs.readFileSync('db/init.sql').toString();
 
 // HOWTO run a raw query
-// const [results] = await sequelize.query(initSql);
-// console.log(results);
+const [results] = await sequelize.query(initSql);
+console.log(results);
 
 const [results_two] = await sequelize.query('SELECT * FROM products');
 console.log(results_two);
--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,4 @@
-drop table if exists products
-create table products (
+create table if not exists products (
     id integer,
     model_name varchar(100),
     brand varchar(100),
@@ -18,7 +17,7 @@ values
     (6, 'iPhone 14', 'Apple', 499 , 'GB'),
     (7, 'Galaxy', 'Samsung', 600, 'DE'),
     (8, 'iPhone 14', 'Apple', 599, 'GB'),
-    (9, 'iPhone 14', 'Apple', 699, 'DE'),
+    (9, 'iPhone 14', 'Apple', 699, 'DE');
 
 create table laptops (
     id integer,
@@ -32,4 +31,4 @@ insert into
     laptops (id, model_name, brand, price, country)
 values
     (1, 'Macbook Pro 14 inch', 'Apple', 1899, 'GB'),
-    (2, 'Microsoft Surface Pro 6', 'Microsoft', 1399, 'DE'),
+    (2, 'Microsoft Surface Pro 6', 'Microsoft', 1399, 'DE');
