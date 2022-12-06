a12d3126c42dbe8e629e1385da41aa17fcbdded9
silvabacc <47926269+silvabacc@users.noreply.github.com>
2022-12-06 12:59:38 +0000

    1 binaries uploaded

--- db/db.js
+++ db/db.js
@@ -1,4 +1,4 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, DataTypes } from 'sequelize';
 import fs from 'fs';
 
 // setup a new database
@@ -8,11 +8,31 @@ const sequelize = new Sequelize('database', '', '', {
   logging: false,
 });
 
-const initSql = fs.readFileSync('db/init.sql').toString();
+const phones = sequelize.define('phones', {
+  model_id: DataTypes.INTEGER,
+  model_name: DataTypes.STRING,
+  brand: DataTypes.STRING,
+  price: DataTypes.DECIMAL,
+  country: DataTypes.STRING,
+});
 
-// HOWTO run a raw query
-const [results] = await sequelize.query(initSql);
-console.log(results);
+sequelize.sync();
 
-const [results_two] = await sequelize.query('SELECT * FROM products');
+phones.create({
+  model_id: 1,
+  model_name: 'iPhone 14',
+  brand: 'Apple',
+  price: 123,
+  country: 'GB',
+});
+
+console.log(phones === sequelize.models.phones); // true
+
+// const initSql = fs.readFileSync('db/init.sql').toString();
+
+// // HOWTO run a raw query
+// const [results] = await sequelize.query(initSql);
+// console.log(results);
+
+const [results_two] = await sequelize.query('SELECT * FROM phones');
 console.log(results_two);
--- db/init.sql
+++ db/init.sql
@@ -1,34 +1,23 @@
-create table if not exists products (
-    id integer,
-    model_name varchar(100),
-    brand varchar(100),
-    price integer
-    country varchar(100)
-);
-
 insert into
-    phones (id, model_name, brand, price, country)
+  phones (model_id, model_name, brand, price, country)
 values
-    (1, 'iPhone 12', 'Apple', 999, 'GB'),
-    (2, 'iPhone 13', 'Apple', 899, 'FR'),
-    (3, 'Pixel', 'Google', 1300, 'GB'),
-    (4, 'iPhone 13', 'Apple', 799, 'GB'),
-    (5, 'Galaxy', 'Samsung', 800), 'GB'),
-    (6, 'iPhone 14', 'Apple', 499 , 'GB'),
-    (7, 'Galaxy', 'Samsung', 600, 'DE'),
-    (8, 'iPhone 14', 'Apple', 599, 'GB'),
-    (9, 'iPhone 14', 'Apple', 699, 'DE');
-
-create table laptops (
-    id integer,
-    model_name varchar(100),
-    brand varchar(100),
-    price integer
-    country varchar(100)
-);
-
+  (1, 'iPhone 12', 'Apple', 999, 'GB'),
+  (2, 'iPhone 13', 'Apple', 899, 'FR'),
+  (3, 'Pixel', 'Google', 1300, 'GB'),
+  (4, 'iPhone 13', 'Apple', 799, 'GB'),
+  (5, 'Galaxy', 'Samsung', 800, 'PO'),
+  (6, 'iPhone 14', 'Apple', 499, 'GB'),
+  (7, 'Galaxy', 'Samsung', 600, 'DE'),
+  (8, 'iPhone 14', 'Apple', 599, 'GB'),
+  (9, 'iPhone 14', 'Apple', 699, 'DE');
 insert into
-    laptops (id, model_name, brand, price, country)
+  laptops (id, model_name, brand, price, country)
 values
-    (1, 'Macbook Pro 14 inch', 'Apple', 1899, 'GB'),
-    (2, 'Microsoft Surface Pro 6', 'Microsoft', 1399, 'DE');
+  (1, 'Macbook Pro 14 inch', 'Apple', 1899, 'GB'),
+  (
+    2,
+    'Microsoft Surface Pro 6',
+    'Microsoft',S
+    1399,
+    'DE'
+  );
