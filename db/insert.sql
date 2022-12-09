8c92cd2d52615a9d4aa4dd6978b161d6a5df56a0
silvabacc <47926269+silvabacc@users.noreply.github.com>
2022-12-09 11:54:35 +0000

    2 files modified

--- db/insert.sql
+++ db/insert.sql
@@ -2,10 +2,10 @@ drop table if exists total_sales;
 drop table if exists phones; 
 
 create table total_sales (model_id, revenue, units);
-create table phones (model_id, model_name, brand, price, country);
+create table phones (model_id, model_name, brand, price, country, period);
 
 insert into
-  total_sales (model_id, total_revenue, total_units)
+  total_sales (model_id, revenue, units)
 values
   (94, 72000.12, 168),
   (27, 444795.32, 946),
@@ -13,18 +13,18 @@ values
   (45, 81570.34, 537),
   (66, 151121.98, 48);
 
-insert into
-  phones (model_id, model_name, brand, price, country)
+insert into 
+  phones (model_id, model_name, brand, price, country, period)
 values
-  (94, 'iPhone 12', 'Apple', 999, 'GB'),
-  (27, 'iPhone 13', 'Apple', 899, 'FR'),
-  (100, 'Pixel', 'Google', 1300, 'GB'),
-  (27, 'iPhone 13', 'Apple', 799, 'GB'),
-  (45, 'Galaxy', 'Samsung', 800, 'PO'),
-  (66, 'iPhone 14', 'Apple', 499, 'GB'),
-  (45, 'Galaxy', 'Samsung', 600, 'DE'),
-  (66, 'iPhone 14', 'Apple', 599, 'GB'),
-  (66, 'iPhone 14', 'Apple', 699, 'DE');
+  (94, 'iPhone 12', 'Apple', 999, 'GB', 1),
+  (27, 'iPhone 13', 'Apple', 899, 'FR', 1),
+  (100, 'Pixel', 'Google', 1300, 'GB', 1),
+  (27, 'iPhone 13', 'Apple', 799, 'GB', 1),
+  (45, 'Galaxy', 'Samsung', 800, 'PO', 2),
+  (66, 'iPhone 14', 'Apple', 499, 'GB', 1),
+  (45, 'Galaxy', 'Samsung', 600, 'DE', 2),
+  (66, 'iPhone 14', 'Apple', 599, 'GB', 2),
+  (66, 'iPhone 14', 'Apple', 699, 'DE', 2);
   
 -- Write a query that aggregates all sales of smartphones in GB
 -- Extend this query to involve all smartphones that are only iPhone 14
